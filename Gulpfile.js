cd9131ce1cf2dc084a7acef512a4f39e7fe2c748
Benny Johansson <benny.johansson@reqtest.com>
2015-09-05 20:51:36 +0100

    Add Roboto font

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,15 +2,18 @@ var gulp       = require('gulp'),
     browserify = require('gulp-browserify');
 
 gulp.task('scripts', function () {
-
     gulp.src(['app/main.js'])
         .pipe(browserify({
             debug: true,
             transform: [ 'reactify' ]
         }))
         .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('styles', function () {
+    gulp.src(['app/style.css'])
+        .pipe(gulp.dest('./public/'));
 
 });
 
-gulp.task('default', ['scripts']);
-
+gulp.task('default', ['scripts', 'styles']);
