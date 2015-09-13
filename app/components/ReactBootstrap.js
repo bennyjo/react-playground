9ab021e1069e71abc5a0b0d0dc1f0dcbc489b0f7
Benny Johansson <benny.johansson@reqtest.com>
2015-09-13 14:51:52 +0100

    Top navigation menu

--- app/components/ReactBootstrap.js
+++ app/components/ReactBootstrap.js
@@ -1,18 +1,30 @@
 var React = require('react');
 var rbs = require('react-bootstrap');
 
-var Button = rbs.Button;
+var Navbar = rbs.Navbar;
+var Nav = rbs.Nav;
+var NavDropdown = rbs.NavDropdown;
+var MenuItem = rbs.MenuItem;
 
 var ReactApp = React.createClass({
-    
+
     componentDidMount: function() {
       console.log('componentDidMount');
     },
 
     render: function() {
         return (
-          <div>
-            <Button bsStyle="primary" bsSize="large">:D</Button>
+          <div className='buzzbattery'>
+            <Navbar brand={<span><img src='/images/buzzbattery-logo-small.png' title='buzzbattery' />buzzbattery</span>} >
+              <Nav right eventKey={0}>
+                <NavDropdown eventKey={1} title="[avatar] username" id="collapsible-navbar-dropdown">
+                  <MenuItem eventKey="1">Profile</MenuItem>
+                  <MenuItem eventKey="2">Billing</MenuItem>
+                  <MenuItem divider />
+                  <MenuItem eventKey="3">Logout</MenuItem>
+                </NavDropdown>
+              </Nav>
+            </Navbar>
           </div>
         )
     }
