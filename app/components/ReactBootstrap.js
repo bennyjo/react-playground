3766e9e69bfb3b130cce747803190e5b7cac0f70
Benny Johansson <benny.johansson@reqtest.com>
2015-09-13 21:49:08 +0100

    Separate TopNavigation module

--- app/components/ReactBootstrap.js
+++ app/components/ReactBootstrap.js
@@ -1,10 +1,7 @@
 var React = require('react');
 var rbs = require('react-bootstrap');
 
-var Navbar = rbs.Navbar;
-var Nav = rbs.Nav;
-var NavDropdown = rbs.NavDropdown;
-var MenuItem = rbs.MenuItem;
+var TopNavigation = require('./TopNavigation');
 
 var Tabs = rbs.Tabs;
 var Tab = rbs.Tab;
@@ -18,16 +15,7 @@ var ReactApp = React.createClass({
     render: function() {
         return (
           <div className='buzzbattery'>
-            <Navbar brand={<span><img src='/images/buzzbattery-logo-small.png' title='buzzbattery' />buzzbattery</span>} >
-              <Nav right eventKey={0}>
-                <NavDropdown eventKey={1} title="[avatar] username" id="collapsible-navbar-dropdown">
-                  <MenuItem eventKey="1">Profile</MenuItem>
-                  <MenuItem eventKey="2">Billing</MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey="3">Logout</MenuItem>
-                </NavDropdown>
-              </Nav>
-            </Navbar>
+            <TopNavigation />
 
             <Tabs className="container" defaultActiveKey={2} position="left" standalone={false} animation={false} tabWidth={3}>
               <Tab eventKey={1} title="Battery 1">Tab 1 content</Tab>
